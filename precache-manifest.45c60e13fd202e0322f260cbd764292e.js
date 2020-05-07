f136d67ce3337bc746e8d5c10842ab833c17cd4b
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-05-07 21:51:09 +0400

    Updates

---
+++ precache-manifest.45c60e13fd202e0322f260cbd764292e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0d57fd08f4f3fc0df52d5509330b49",
+    "url": "/raect_movie_posters/index.html"
+  },
+  {
+    "revision": "debd2c10a118003cb02f",
+    "url": "/raect_movie_posters/static/css/main.46fff7c5.chunk.css"
+  },
+  {
+    "revision": "65d9391f716933fbe275",
+    "url": "/raect_movie_posters/static/js/2.5d746114.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/raect_movie_posters/static/js/2.5d746114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debd2c10a118003cb02f",
+    "url": "/raect_movie_posters/static/js/main.e302924b.chunk.js"
+  },
+  {
+    "revision": "11104f896d9a16af8b79",
+    "url": "/raect_movie_posters/static/js/runtime-main.eafd6413.js"
+  },
+  {
+    "revision": "4a5d995135583d7aa6089c6c0d68ad78",
+    "url": "/raect_movie_posters/static/media/loader.4a5d9951.svg"
+  }
+]);
