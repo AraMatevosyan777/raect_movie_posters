11f0e8a6bfa552857d2616a7a3fe5a29f9b8bbf2
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-05-08 13:05:06 +0400

    deploy my app

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import ContentContainer from './components/Content/ContentContainer';
-import {Route, Redirect} from 'react-router-dom';
+import {Route, withRouter, Redirect} from 'react-router-dom';
 
 const App = () => {
     
     return(
       <div className="App">
         <Header />
-        {/* <Route path='' render={()=> <Redirect to='./posters'/>}/> */}
-        <Route path='' render={()=> <ContentContainer/>}/>
+        <Route path='/' render={()=> <Redirect to='/poster'/>}/>
+        <Route path='/poster' render={()=> <ContentContainer/>}/>
       </div>
     );
 }
 
 
 
-export default App;
+export default withRouter(App);
