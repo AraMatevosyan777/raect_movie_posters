8ccd8e26b313b91e951e202d569f7b802cb5706c
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-05-08 12:57:25 +0400

    deploy my app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import ContentContainer from './components/Content/ContentContainer';
+import {Route, Redirect} from 'react-router-dom';
 
 const App = () => {
     
     return(
       <div className="App">
         <Header />
-        <ContentContainer/>
+        {/* <Route path='' render={()=> <Redirect to='./posters'/>}/> */}
+        <Route path='' render={()=> <ContentContainer/>}/>
       </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <HashRouter>
+      <Provider store={store}>
         <App />
-    </Provider>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
